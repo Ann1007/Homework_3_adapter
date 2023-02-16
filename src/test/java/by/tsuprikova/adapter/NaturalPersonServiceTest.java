3ca35bb60e53117d184c5d5fa3a9c5ae096e3248
Ann1007 <annarabota1007@mail.ru>
2023-02-16 14:31:53 +0300

    изменение тестов для natural person request service

--- src/test/java/by/tsuprikova/adapter/NaturalPersonServiceTest.java
+++ src/test/java/by/tsuprikova/adapter/NaturalPersonServiceTest.java
@@ -1,5 +1,6 @@
 package by.tsuprikova.adapter;
 
+import by.tsuprikova.adapter.exceptions.ResponseWithFineNullException;
 import by.tsuprikova.adapter.model.NaturalPersonRequest;
 import by.tsuprikova.adapter.model.ResponseWithFine;
 import by.tsuprikova.adapter.service.NaturalPersonRequestService;
@@ -7,21 +8,21 @@ import by.tsuprikova.adapter.service.impl.NaturalPersonRequestServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
@@ -30,12 +31,11 @@ import java.util.UUID;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class NaturalPersonServiceTest {
-
 
     @LocalServerPort
     private int port;
-
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -44,6 +44,8 @@ public class NaturalPersonServiceTest {
 
     private NaturalPersonRequestService requestService;
 
+    private NaturalPersonRequest naturalPersonRequest;
+
     @BeforeAll
     static void beforeAll() throws IOException {
         mockWebServer = new MockWebServer();
@@ -51,11 +53,13 @@ public class NaturalPersonServiceTest {
     }
 
     @BeforeEach
-    void setup(){
+    void setup() {
 
         WebClient webClient = WebClient.builder().baseUrl(mockWebServer.url("/smv").toString()).build();
         requestService = new NaturalPersonRequestServiceImpl(webClient);
-
+        naturalPersonRequest = new NaturalPersonRequest();
+        String sts = "59 ут 123456";
+        naturalPersonRequest.setSts(sts);
 
     }
 
@@ -66,19 +70,17 @@ public class NaturalPersonServiceTest {
 
 
     @Test
-    void transferRequestTest() throws Exception {
-
-        NaturalPersonRequest naturalPersonRequest = new NaturalPersonRequest();
-        String sts = "59 ут 123456";
-        naturalPersonRequest.setSts(sts);
+    void transferValidNaturalPersonRequestTest() throws Exception {
 
         MockResponse response = new MockResponse().setBody(objectMapper.writeValueAsString(naturalPersonRequest)).
-                setResponseCode(200).
+                setResponseCode(202).
                 addHeader("Content-Type", "application/json");
 
         mockWebServer.enqueue(response);
-        Mono<NaturalPersonRequest> resultMono = requestService.transferClientRequest(naturalPersonRequest);
-        assertThat(resultMono.block().getSts(), is(sts));
+        ResponseEntity<NaturalPersonRequest> result = requestService.transferClientRequest(naturalPersonRequest);
+
+        assertThat(result.getBody().getSts(), is("59 ут 123456"));
+        assertThat(result.getStatusCode(), is(HttpStatus.ACCEPTED));
 
     }
 
@@ -86,39 +88,50 @@ public class NaturalPersonServiceTest {
     @Test
     void getResponseIsNotNullTest() throws Exception {
 
-        NaturalPersonRequest naturalPersonRequest = new NaturalPersonRequest();
-        String sts = "59 ут 123456";
-        naturalPersonRequest.setSts(sts);
-
         ResponseWithFine responseWithFine = new ResponseWithFine();
         BigDecimal amountOfAccrual = new BigDecimal(28);
         BigDecimal amountOfPaid = new BigDecimal(28);
         int numberOfResolution = 321521;
         String articleOfKoap = "21.3";
-        responseWithFine.setSts(sts);
+        responseWithFine.setSts("59 ут 123456");
         responseWithFine.setAmountOfAccrual(amountOfAccrual);
         responseWithFine.setArticleOfKoap(articleOfKoap);
         responseWithFine.setAmountOfPaid(amountOfPaid);
         responseWithFine.setNumberOfResolution(numberOfResolution);
 
-        ResponseEntity<ResponseWithFine> res = new ResponseEntity<>(responseWithFine, HttpStatus.OK);
-
-        MockResponse retryResponse = new MockResponse().setResponseCode(500);
-        mockWebServer.enqueue(retryResponse);
-
-        MockResponse mockResponse = new MockResponse().setBody(objectMapper.writeValueAsString(res)).
+        MockResponse mockResponse = new MockResponse().setBody(objectMapper.writeValueAsString(responseWithFine)).
                 setResponseCode(200).
                 addHeader("Content-Type", "application/json");
 
         mockWebServer.enqueue(mockResponse);
-        Mono<ResponseEntity<ResponseWithFine>> resultResponse = requestService.getResponse(naturalPersonRequest);
+        ResponseEntity<ResponseWithFine> resultResponse = requestService.getResponse(naturalPersonRequest);
 
-        assertThat(resultResponse.block().getStatusCode(), is(HttpStatus.OK));
-
+        assertThat(resultResponse.getStatusCode(), is(HttpStatus.OK));
+        assertThat(resultResponse.getBody().getSts(), is("59 ут 123456"));
 
 
     }
 
+
+    @Test
+    void getResponseIsNullTest() throws Exception {
+
+        MockResponse mockResponse = new MockResponse().
+                setResponseCode(404);
+
+        mockWebServer.enqueue(mockResponse);
+        mockWebServer.enqueue(mockResponse);
+        mockWebServer.enqueue(mockResponse);
+        mockWebServer.enqueue(mockResponse);
+
+        ResponseWithFineNullException thrown = assertThrows(ResponseWithFineNullException.class, () -> {
+            requestService.getResponse(naturalPersonRequest);
+        });
+
+        String errorMessage = "No information found for  59 ут 123456";
+        Assertions.assertEquals(errorMessage, thrown.getMessage());
+
+    }
 
 
     @Test
@@ -131,8 +144,8 @@ public class NaturalPersonServiceTest {
                 addHeader("Content-Type", "application/json");
 
         mockWebServer.enqueue(response);
-        ResponseEntity<Void> result = requestService.deleteResponse(id);
-        assertThat(result.getStatusCode(), is(HttpStatus.OK));
+        requestService.deleteResponse(id);
+
 
     }
 
